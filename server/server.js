1e677056142f4868a2ef3e64fd68aac158357381
Lige Liu <xtllg@mit.edu>
2020-01-17 23:29:33 -0500

    Added Database

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ const socket = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://xtllg:kllsbjf@M314159B@cluster0-i9gqe.mongodb.net/test?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "Treasure Map";
 
 // connect to mongodb
 mongoose
