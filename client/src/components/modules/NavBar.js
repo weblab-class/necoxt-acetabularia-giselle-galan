b759a3eae184968d102c51205e65eb179454f471
Lige Liu <xtllg@mit.edu>
2020-01-21 17:07:08 -0500

    configure google login

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -5,7 +5,8 @@ import GoogleLogin, { GoogleLogout } from "react-google-login";
 import "./NavBar.css";
 
 // This identifies your web application to Google's authentication service
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+// const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "387931991633-7v7bv90k3t89b12vr5lhlf7369tolad1.apps.googleusercontent.com";
 
 /**
  * The navigation bar at the top of all pages. Takes no props.
