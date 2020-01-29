9b93bda07f88bc6f51c667ec8ab0d682fe900344
Lige Liu <xtllg@mit.edu>
2020-01-29 11:44:31 -0500

    submission for beta version

--- client/src/components/modules/Created.js
+++ client/src/components/modules/Created.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 import "./Created.css";
+import { get } from "../../utilities";
+import { post } from "../../utilities";
+import SingleMapCard from "./SingleMapCard.js";
 
 /**
  * AboutMe
@@ -9,69 +12,41 @@ import "./Created.css";
 class Created extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      treasureMapsData: [],
+    }
+  }
+
+  componentDidMount() {
+    // get map content (all maps) 
+    get("/api/created_treasure", this.props.user).then((treasureMapsDataObjs) => {
+      this.setState({ treasureMapsData: treasureMapsDataObjs })
+    });
   }
 
   render() {
-    return (
-
-<div class="article-row-section">
-  <div class="article-row-section-inner">
-
-    <h2 class="article-row-section-header">News</h2>
-
-    <a href="#">
-      <article class="article-row">
-        <div class="article-row-img">
-          <img src="https://placehold.it/200" alt="picture of a whale eating a donkey" />
+    // console.log(this.props.user);
+    if (this.state.treasureMapsData.length !== 0) {
+      console.log("test",this.state.treasureMapsData)
+      return (
+        <div className="cardContainer">
+          {this.state.treasureMapsData.map((treasureMapContent) => (
+            <SingleMapCard 
+              treasure_id={treasureMapContent._id} 
+              creator_name={treasureMapContent.creator_name}
+              // total_steps={treasureMapContent.treasureSteps.length}
+            />
+          ))}
         </div>
-        <div class="article-row-content">
-          <h1 class="article-row-content-header">HOW SPENDING $162,301.42 ON CLOTHES MADE ME $692,500</h1>
-
-          <p class="article-row-content-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quia sit ullam, assumenda et. est at. Minima cum enim, vero eligendi perspiciatis similique modi voluptatem officia fugiat.</p>
-
-          <p class="article-row-content-author">By Yeti</p>
-          <time class="article-row-content-time" datetime="2008-02-14 20:00">July 14th 2021</time>
+      );
+    } else {
+      return (
+        <div className="cardContainer">
+          <p>Haven't created any map yet</p>
         </div>
-      </article>
-    </a>
+      );
+    }
 
-    <a href="#">
-      <article class="article-row article-row-reversed">
-        <div class="article-row-img">
-          <img src="https://placehold.it/200" alt="picture of a whale eating a donkey" />
-        </div>
-        <div class="article-row-content">
-          <h1 class="article-row-content-header">HOW TO QUIT YOUR JOB, MOVE TO PARADISE AND GET PAID TO CHANGE THE WORLD</h1>
-
-          <p class="article-row-content-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore accusamus sint quas, odit, enim architecto officiis culpa!</p>
-
-          <p class="article-row-content-author">By Yeti</p>
-          <time class="article-row-content-time" datetime="2008-02-14 20:00">July 14th 2021</time>
-        </div>
-      </article>
-    </a>
-
-    <a href="#">
-      <article class="article-row">
-        <div class="article-row-img">
-          <img src="https://placehold.it/200" alt="picture of a whale eating a donkey" />
-        </div>
-        <div class="article-row-content">
-          <h1 class="article-row-content-header">HOW I’M GOING TO LOSE ALL MY CUSTOMERS AND WRECK MY BUSINESS</h1>
-
-          <p class="article-row-content-description">Lorem ipsum dolor sit amet, consectetur cupiditate, unde libero quisquam ipsam debitis earum omnis aperiam nulla eaque vitae error optio tempora voluptatem, quae impedit laborum placeat. Expedita!</p>
-
-          <p class="article-row-content-author">By Yeti</p>
-          <time class="article-row-content-time" datetime="2008-02-14 20:00">July 14th 2021</time>
-        </div>
-      </article>
-    </a>
-
-  </div>
-</div>
-
-
-    );
   }
 }
 
--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 import React, { Component } from "react";
+import ContentEditable from 'react-contenteditable';
+import moment from 'moment'; 
+// moment.locale('us'); 
 import NavBar from "../modules/NavBar.js";
 // import GoogleLogin, { GoogleLogout } from "react-google-login";
 import { get } from "../../utilities";
 import { post } from "../../utilities";
-import AboutMe from "../modules/AboutMe.js";
+import Created from "../modules/Created.js";
 
 
 import "../../utilities.css";
@@ -16,41 +19,93 @@ import "../foundation.css";
  * 
  * 
  */
+const defaultDescription = "<p>Somewhere on the earth</p>";
+const defaultTitle = "SONG OF MYSELF";
+const defaultContent = "<p>I celebrate myself, and sing myself,<br />And what I assume you shall assume,<br />For every atom belonging to me as good belongs to you.<br /></p><p>I loafe and invite my soul,<br />I lean and loafe at my ease observing a spear of summer grass.<br /></p><p>My tongue, every atom of my blood, form’d from this soil, this air,<br />Born here of parents born here from parents the same, and their parents the same,<br />I, now thirty-seven years old in perfect health begin,<br />Hoping to cease not till death.</p><p>Creeds and schools in abeyance,<br />Retiring back a while sufficed at what they are, but never forgotten,<br />I harbor for good or bad, I permit to speak at every hazard,<br />Nature without check with original energy.</p>";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
+    this.contentEditable_1 = React.createRef();
+    this.contentEditable_2 = React.createRef();
+    this.contentEditable_3 = React.createRef();
     // Initialize Default State
     this.state = {
+      user: null,
       viewerId: null,
-      profileName: null,
-      profileId: null,
-      profileDescription: null,
+      profileDescription: defaultDescription,
+      aboutMe: {
+        date: moment().format("MMMM Do YYYY, h:mm:ss a"),
+        title: defaultTitle,
+        content: defaultContent,
+      },
       tagSelection: "AboutMe",
+      profileModified: false,
     };
   }
 
   componentDidMount() {
     document.title = "User Profile";
-    // console.log(1, this.props.userId);
+    // console.log(1, this.props.viewerId);
     get("/api/whoami").then((user) => {
+      // console.log(1, user);
       if (user._id) {
         this.setState({ viewerId: user._id });
       }
     });
     get("/api/user", {_id: this.props.userId}).then((user) => {
-      // console.log(2, user);
+      // console.log(user);
+      let profileDescription = this.state.profileDescription;
+      let aboutMe = this.state.aboutMe;
+      user[0].profileDescription ? profileDescription = user[0].profileDescription : null;
+      user[0].aboutMe ? aboutMe = user[0].aboutMe : null;
       this.setState({
-        profileName: user[0].name,
-        profileId: this.props.userId,
+        user: user[0],
+        profileDescription: profileDescription,
+        aboutMe: aboutMe,
       });
     });
   }
 
-  changeTag = (newTag) => {
+  changeTag = (newInput) => {
     this.setState({
-      tagSelection: newTag,
+      tagSelection: newInput,
     });
+  }
+
+  changeProfileDescription = (event) => {
+    this.setState({
+      profileDescription: event.target.value,
+      profileModified: true,
+    });
+  }
+
+  changeAboutMeTitle = (event) => {
+    this.setState({aboutMe:
+      {...this.state.aboutMe, date: moment().format("MMMM Do YYYY, h:mm:ss a"), title: event.target.value,},
+      profileModified: true,
+    });
+  }
+
+  changeAboutMeContent = (event) => {
+    this.setState({aboutMe:
+      {...this.state.aboutMe, date: moment().format("MMMM Do YYYY, h:mm:ss a"), content: event.target.value,},
+      profileModified: true,
+    });
+  }
+
+  onSave = () => {
+    this.setState({
+      profileModified: false,
+    });
+    let profileDescription = null;
+    let aboutMe = null;
+    (defaultDescription !== this.state.profileDescription) ? profileDescription = this.state.profileDescription : null;
+    (defaultTitle !== this.state.aboutMe.title && defaultContent === this.state.aboutMe.content) ? aboutMe = this.state.aboutMe : null;
+    post("/api/user_profile", {
+      profileDescription: profileDescription,
+      aboutMe: aboutMe,
+    })
   }
 
   tagClass = (tagName) => {
@@ -61,61 +116,119 @@ class Profile extends Component {
     )
   }
 
+  editableStatus = () => (this.props.userId !== this.state.viewerId);
+
   render() {
-    return (
-      <>
-      <div className="profile-background" style={{"border": "1px dashed",}}>
-      {/* <div className="profile-background"> */}
-        <NavBar 
-          handleLogin={this.props.handleLogin}
-          handleLogout={this.props.handleLogout}
-          userId={this.props.userId}
-        />
-        <div className="profile-avatarContainer" style={{"border": "1px dashed",}}>
-        {/* <div className="profile-avatarContainer"> */}
-          <div className="profile-avatar" style={{"border": "1px dashed",}}>
-          {/* <div className="profile-avatar"> */}
-            <div className="profile-picture-container" style={{"border": "1px red dashed",}}>
-            {/* <div className="profile-picture-container"> */}
-              <div className="profile-picture" style={{"border": "1px red dashed",}} />
-              {/* <div className="profile-picture" /> */}
-            </div>
-            <div className="profile-descriptions" style={{"border": "1px red dashed",}}>
-            {/* <div className="profile-descriptions"> */}
-              {/* <div className="profile-name" style={{"border": "1px red dashed",}}>Tamsen Vlasta</div> */}
-              <div className="profile-name" style={{"border": "1px red dashed",}}>{this.state.profileName}</div>
-              {/* <div className="profile-name">{this.state.profileName}</div> */}
-              <div className="profile-line" style={{"border": "1px red dashed",}}>
-              {/* <div className="profile-line"> */}
-                <p className="pin icon"/> 
-                {this.state.profileDescription 
-                  ? <p className="profile-line-content">{this.state.profileDescription}</p>
-                  : <p className="profile-line-content">Somewhere on the earth</p>
-                }
+    // console.log(this.state);
+    if (this.state.user && this.state.viewerId){
+      return (
+        <>
+        {/* <div className="profile-background" style={{"border": "1px dashed",}}> */}
+        <div className="profile-background">
+          <NavBar 
+            handleLogin={this.props.handleLogin}
+            handleLogout={this.props.handleLogout}
+            userId={this.props.userId}
+          />
+          {/* <div className="profile-avatarContainer" style={{"border": "1px dashed",}}> */}
+          <div className="profile-avatarContainer">
+            {/* <div className="profile-avatar" style={{"border": "1px dashed",}}> */}
+            <div className="profile-avatar">
+              {/* <div className="profile-picture-container" style={{"border": "1px red dashed",}}> */}
+              <div className="profile-picture-container">
+                {/* <div className="profile-picture" style={{"border": "1px red dashed",}} /> */}
+                <div className="profile-picture" />
+              </div>
+              {/* <div className="profile-descriptions" style={{"border": "1px red dashed",}}> */}
+              <div className="profile-descriptions">
+                {/* <div className="profile-name" style={{"border": "1px red dashed",}}>Tamsen Vlasta</div> */}
+                {/* <div className="profile-name" style={{"border": "1px red dashed",}}>{this.state.user.name}</div> */}
+                <div className="profile-name">{this.state.user.name}</div>
+                {/* <div className="profile-line" style={{"border": "1px red dashed",}}> */}
+                <div className="profile-line">
+                  <div className="pin icon"/> 
+                  <div className="profile-line-container">
+                    <ContentEditable 
+                      innerRef={this.contentEditable_1}
+                      disabled={this.editableStatus()}
+                      html={this.state.profileDescription}
+                      onChange = {this.changeProfileDescription}
+                    />
+                  </div>
+                  {/* {console.log(this.state.profileDescription)} */}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="profile-subContainer" style={{"border": "1px dashed",}}>
-          {/* <div className="profile-subContainer"> */}
-            <nav className="hover-underline-menu" style={{"border": "1px dashed",}}>
-            {/* <nav className="hover-underline-menu"> */}
-              <ul className="menu">
-                <li><a className={this.tagClass("AboutMe")} onClick={() => this.changeTag("AboutMe")} href="#">About Me</a></li>
-                <li><a className={this.tagClass("Created")} onClick={() => this.changeTag("Created")} href="#">Created</a></li>
-                <li><a className={this.tagClass("Searched")} onClick={() => this.changeTag("Searched")} href="#">Searched</a></li>
-                <li><a className={this.tagClass("Liked")} onClick={() => this.changeTag("Liked")} href="#">Liked</a></li>
-                {/* <li><a className="underline-from-center" href="#">About Me</a></li>
-                <li><a className="underline-from-center" href="#">Created</a></li>
-                <li><a className="underline-from-center" href="#">Searched</a></li>
-                <li><a className="underline-from-center" href="#">Liked</a></li> */}
-              </ul>
-            </nav>
+            {/* <div className="profile-subContainer" style={{"border": "1px dashed",}}> */}
+            <div className="profile-subContainer">
+              {/* <nav className="hover-underline-menu" style={{"border": "1px dashed",}}> */}
+              <nav className="hover-underline-menu">
+                <ul className="menu">
+                  <li><a className={this.tagClass("AboutMe")} onClick={() => this.changeTag("AboutMe")} href="#">About Me</a></li>
+                  <li><a className={this.tagClass("Created")} onClick={() => this.changeTag("Created")} href="#">Created</a></li>
+                  {/* <li><a className={this.tagClass("Searched")} onClick={() => this.changeTag("Searched")} href="#">Searched</a></li> */}
+                  {/* <li><a className={this.tagClass("Liked")} onClick={() => this.changeTag("Liked")} href="#">Liked</a></li> */}
+                  {this.state.profileModified ? <li><a className="underline-from-center" onClick={() => this.onSave()} href="#">Save</a></li> : null}
+                  {/* <li><a className="underline-from-center" href="#">About Me</a></li>
+                  <li><a className="underline-from-center" href="#">Created</a></li>
+                  <li><a className="underline-from-center" href="#">Searched</a></li>
+                  <li><a className="underline-from-center" href="#">Liked</a></li> */}
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
-      </div>
-      <AboutMe />
-      </>
-    );
+        { this.state.tagSelection == "AboutMe" ? 
+          <div className="simple-article-header grid-container" style={{"border": "transparent"}}>
+            <h4 className="article-title">
+              <ContentEditable 
+                innerRef={this.contentEditable_2}
+                disabled={this.editableStatus()}
+                html={this.state.aboutMe.title}
+                onChange = {this.changeAboutMeTitle}
+              />
+            </h4>
+            <p className="article-date-read">Updated on: {this.state.aboutMe.date}</p>
+            <div className="divider"/>
+            <br/>
+            <div className="article-post-content">
+              <ContentEditable 
+                innerRef={this.contentEditable_3}
+                disabled={this.editableStatus()}
+                html={this.state.aboutMe.content}
+                onChange = {this.changeAboutMeContent}
+              />
+            </div>
+          </div>
+          : null
+        }
+        {this.state.tagSelection == "Created" ? 
+          <Created 
+            user={this.state.user}
+          />
+          : null
+        }
+        {this.state.tagSelection == "Searched" ? 
+          null
+          : null
+        }
+        {this.state.tagSelection == "Liked" ? 
+          null
+          : null
+        }
+        </>
+      );
+    } else {
+      return (
+        <div className="profile-background">
+          <NavBar 
+            handleLogin={this.props.handleLogin}
+            handleLogout={this.props.handleLogout}
+            userId={this.props.userId}
+          />
+        </div>
+      );
+    }
   }
 }
 
